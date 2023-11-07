cd1ae29f3d3441f06c42ee8c73a4926c09e8e2db
jsh9057 <jsh9057@skuniv.ac.kr>
2023-11-07 21:42:03 +0900

    [add] FeignClient Logger

--- src/main/java/dev/be/feign/service/DemoService.java
+++ src/main/java/dev/be/feign/service/DemoService.java
@@ -1,10 +1,13 @@
 package dev.be.feign.service;
 
+import dev.be.feign.config.FeignConfig;
 import dev.be.feign.feign.client.DemoFeignClient;
 import dev.be.feign.feign.common.dto.BaseRequestInfo;
 import dev.be.feign.feign.common.dto.BaseResponseInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DemoService {
     private final DemoFeignClient demoFeignClient;
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(FeignConfig.class);
     public String get() {
         ResponseEntity<BaseResponseInfo> response = demoFeignClient.callGet("CustomHeader", "CustomName", 1L);
         log.info("Name : {}",response.getBody().getName());
@@ -31,6 +35,21 @@ public class DemoService {
         log.info("Name : {}",response.getBody().getName());
         log.info("Age : {}",response.getBody().getAge());
         log.info("Header : {}",response.getBody().getHeader());
+        log.info("-------------------------------------------------------------");
+
+
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            // getBeanDefinition(): Bean 이름으로 Bean의 메타데이터 조회
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            // ROLE.APPLICATION: 개발자가 애플리케이션 개발을 위해 등록한 bean 또는 외부 라이브러리
+            // ROLE.INFRASTRUCTURE: 스프링 내부에서 사용하는 bean
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName); // getBean(): Bean 이름으로 Bean 객체(인스턴스)를 조회
+                System.out.println("name = " + beanDefinitionName + " object: " + bean);
+            }
+        }
         return "post";
     }
 }
